d4a4f529c20c46af655ad978b71fc5c9e90fbfb1
Yemi Coker <88531333+cokerthehacker@users.noreply.github.com>
2024-05-29 14:40:57 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Smooth scrolling for internal links
 document.querySelectorAll('a[href^="#"]').forEach(anchor => {
     anchor.addEventListener('click', function (e) {
         e.preventDefault();
@@ -7,26 +8,20 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
         });
     });
 });
-document.addEventListener('DOMContentLoaded', function () {
-    // Get the menu icon and navigation ul
-    var menuIcon = document.querySelector('.menu-icon');
-    var navUl = document.querySelector('nav ul');
 
-    // Add a click event listener to the menu icon
+document.addEventListener('DOMContentLoaded', function () {
+    // Menu icon toggle functionality
+    const menuIcon = document.querySelector('.menu-icon');
+    const navUl = document.querySelector('nav ul');
+
     menuIcon.addEventListener('click', function () {
-        // Toggle the 'show' class on the navigation ul
         navUl.classList.toggle('show');
     });
-});
-document.addEventListener('DOMContentLoaded', function () {
-    // Your date of birth in the format: YYYY-MM-DD
-    var dob = new Date('2004-04-23');
 
-    // Current date
-    var currentDate = new Date();
-
-    // Calculate age
-    var age = currentDate.getFullYear() - dob.getFullYear();
+    // Calculate and display age
+    const dob = new Date('2004-04-23');
+    const currentDate = new Date();
+    let age = currentDate.getFullYear() - dob.getFullYear();
 
     // Check if birthday has occurred this year
     if (currentDate.getMonth() < dob.getMonth() || (currentDate.getMonth() === dob.getMonth() && currentDate.getDate() < dob.getDate())) {
